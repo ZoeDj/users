395b7907221f82c4b99113949e997b70979320da
ZoeDj <studio3fun@gmail.com>
2019-09-27 09:32:21 -0400

    made it dynamic

--- src/Users/users.js
+++ src/Users/users.js
@@ -4,19 +4,38 @@ import User from "./user";
 class Users extends Component {
   state = {
     users: [
-      { name: "John", age: 20 },
-      { name: "Nick", age: 30 },
-      { name: "Mark", age: 40 }
+      { name: "John", age: 51 },
+      { name: "Nick", age: 43 },
+      { name: "Mark", age: 37 },
+      { name: "Vince", age: 34 },
+      { name: "Bob", age: 42 },
+      { name: "Brian", age: 57 }
     ],
     title: "Users List"
   };
+
+  makeMeYounger = () => {
+    const newState = this.state.users.map(user => {
+      const tempUser = user;
+      tempUser.age -= 10;
+      return tempUser;
+    });
+    this.setState({
+      newState
+    });
+  };
+
   render() {
     return (
       <div>
+        <button onClick={this.makeMeYounger}>Make us 10 years younger</button>
+        <br />
         <h1>{this.state.title}</h1>
-        <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
-        <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
-        <User age={this.state.users[2].age}>{this.state.users[2].name}</User>
+        {this.state.users.map(user => (
+          <User key={user.name} age={user.age}>
+            {user.name}
+          </User>
+        ))}
       </div>
     );
   }
